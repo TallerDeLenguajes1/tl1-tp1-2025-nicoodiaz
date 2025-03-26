4166956e1c14622e5b1fef8191c12bc8e5dffac5
Nico Diaz <nicolas0405.nd@gmail.com>
2025-03-26 04:59:06 +0000

    Ejercicio 3 del tp1

--- tp1_1.c
+++ tp1_1.c
@@ -4,7 +4,23 @@
 
 int main() 
 {
-    printf("Hola Mundo!");
+    //Declaro variable y puntero y le asigno la direccion de memoria
+    int variable = 3;
+    int *pVariable = &variable;
+
+    //e) 1)
+    printf("Contenido del puntero: %d \n", *pVariable);
+    //printf("Contenido del puntero: %d \n", *pVariable);
+
+    //e) 2)
+    printf("Direccion de memoria almacenada por el puntero: %p \n", pVariable);
+    //e) 3)
+    printf("Direccion de memoria de la variable: %p \n", &variable);
+    //e) 4)
+    printf("Direccion de memoria del puntero: %p \n", &pVariable);
+    //e) 5)
+    printf("Tamaño de la memoria en Bytes de: variable %ld \n", sizeof(variable));
+
 
     return 0;
 }
